e9caf1bb53964bd47107f9a981503d14a74bfc83
Rafal Wojdyla <ravwojdyla@gmail.com>
2025-06-28 15:04:45 -0700

    Handle N in the loss function

--- src/tests/caduceus_distillation_test.py
+++ src/tests/caduceus_distillation_test.py
@@ -4,14 +4,21 @@ import pytest
 import torch
 import torch.nn.functional as F
 
-from src.caduceus_distillation import distillation_loss
+from src.caduceus_distillation import (
+    CADUCEUS_NON_SPECIFIC_NUCLEOTIDE_TOKEN_ID,
+    _filter_non_specific_nucleotides_and_batch,
+    distillation_loss,
+)
 
 
 @pytest.fixture
 def basic_inputs():
-    student = torch.randn(2, 5, 10)
-    teacher = torch.randn(2, 5, 10)
-    targets = torch.randint(0, 10, (2, 5))
+    B, S, V = 2, 5, 16  # Batch size, Sequence length, Vocabulary size
+    student = torch.randn(B, S, V)
+    teacher = torch.randn(B, S, V)
+    # 7 to 10 represent the valid nucleotide token IDs, it doesn't really matter for the test
+    # but we do test some `non-specific` nucleotide (token ID 11)
+    targets = torch.randint(7, 11, (B, S))
     return student, teacher, targets
 
 
@@ -44,9 +51,7 @@ def test_temperature_scaling(basic_inputs):
     hard_loss = F.cross_entropy(
         (student / small_temp).view(-1, student.size(-1)),
         torch.argmax(teacher, dim=-1).view(-1),
-        # NOTE: match the `batchmean`` behavior of KL divergence, first sum then divide by batch size
-        reduction="sum",
-    ) / (student.size(0) * student.size(1))
+    )
     assert torch.isclose(loss_low_temp, hard_loss)
 
 
@@ -60,3 +65,47 @@ def test_invalid_inputs(basic_inputs):
         distillation_loss(student, teacher, targets, temperature=-1.0)
     with pytest.raises(AssertionError, match=re.escape("Alpha must be in [0, 1]")):
         distillation_loss(student, teacher, targets, alpha=1.1)
+
+
+def test_filter_non_specific_nucleotides(basic_inputs):
+    student, teacher, targets = basic_inputs
+
+    original_num_targests = targets.numel()
+    # randomly set 10% of the targets to a special token CADUCEUS_NON_SPECIFIC_NUCLEOTIDE_TOKEN_ID
+    num_non_specific = int(0.1 * targets.numel())
+    non_specific_indices = torch.randperm(targets.numel())[:num_non_specific]
+    targets.view(-1)[non_specific_indices] = CADUCEUS_NON_SPECIFIC_NUCLEOTIDE_TOKEN_ID
+
+    assert torch.any(targets == CADUCEUS_NON_SPECIFIC_NUCLEOTIDE_TOKEN_ID)
+
+    assert student.ndim == 3
+    assert teacher.ndim == 3
+    assert targets.ndim == 2
+
+    student, teacher, targets = _filter_non_specific_nucleotides_and_batch(
+        student, teacher, targets
+    )
+
+    # Check that the non-specific nucleotide token is filtered out
+    assert not torch.any(targets == CADUCEUS_NON_SPECIFIC_NUCLEOTIDE_TOKEN_ID)
+    assert student.ndim == 2
+    assert teacher.ndim == 2
+    assert targets.ndim == 1
+    assert student.size(0) == teacher.size(0) == targets.size(0)
+    assert student.size(1) == teacher.size(1)
+
+    assert original_num_targests - num_non_specific == student.size(0)
+
+
+def test_filter_non_specific_nucleotides_filter_all(basic_inputs):
+    student, teacher, targets = basic_inputs
+
+    # Set all targets to a non-specific nucleotide token
+    targets.fill_(CADUCEUS_NON_SPECIFIC_NUCLEOTIDE_TOKEN_ID)
+
+    student, teacher, targets = _filter_non_specific_nucleotides_and_batch(
+        student, teacher, targets
+    )
+
+    assert targets.size(0) == 0
+    assert student.size(0) == 0
