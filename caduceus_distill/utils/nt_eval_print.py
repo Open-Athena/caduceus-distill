4cbf436d662f5882b3f792f49712bf46bec0c1fa
Rafal Wojdyla <ravwojdyla@gmail.com>
2025-07-09 15:30:24 -0700

    wip

--- caduceus_distill/utils/nt_eval_print.py
+++ caduceus_distill/utils/nt_eval_print.py
@@ -1,5 +1,6 @@
 from typing import Annotated
 
+import fsspec
 import pandas as pd
 import typer
 
@@ -10,20 +11,42 @@ def set_index(df: pd.DataFrame) -> pd.DataFrame:
 
 def main(
     experiment_path: Annotated[
-        str, typer.Argument(help="Path to the experiment result CSV file")
-    ],
+        str | None, typer.Argument(help="Path to the experiment result CSV file")
+    ] = None,
+    output_dir: Annotated[
+        str, typer.Option(help="Result directory")
+    ] = "gs://cadu-distill/nt_eval",
 ) -> None:
-    random_results = set_index(
-        pd.read_csv("gs://cadu-distill/nt_eval/random.csv")
-    ).rename(columns={"value": "random"})
-    teacher_results = set_index(
-        pd.read_csv("gs://cadu-distill/nt_eval/teacher.csv")
-    ).rename(columns={"value": "teacher"})
-    experiment_results = set_index(pd.read_csv(experiment_path)).rename(
-        columns={"value": "experiment"}
+    df = set_index(pd.read_csv(f"{output_dir}/random.csv")).rename(
+        columns={"value": "random"}
+    )
+    teacher_results = set_index(pd.read_csv(f"{output_dir}/teacher.csv")).rename(
+        columns={"value": "teacher"}
     )
 
-    print(random_results.join(experiment_results).join(teacher_results))
+    if experiment_path is None:
+        fs = fsspec.filesystem("gs")
+        for e in fs.ls(output_dir):
+            if not e.endswith(".csv"):
+                continue
+            if e.endswith("random.csv") or e.endswith("teacher.csv"):
+                continue
+
+            df = df.join(
+                set_index(pd.read_csv(f"gs://{e}")).rename(
+                    columns={"value": f"val_{e.split('.csv')[0].split('_ckpt')[0].split('_val_loss_total_')[1]}"}
+                )
+            )
+    else:
+
+        df = df.join(
+            set_index(pd.read_csv(experiment_path)).rename(
+                columns={"value": "experiment"}
+            )
+        )
+    df = df.join(teacher_results)
+    df.reset_index().to_markdown("/tmp/p.md")
+    print(df)
 
 
 if __name__ == "__main__":
